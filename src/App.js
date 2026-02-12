57c391482489302fbea2cc333ec51de6791e0385
nikkysmile <34791404+nikkysmile@users.noreply.github.com>
2026-02-12 15:39:55 +0700

    Replace Certificates import with Projects import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
-import Certificates from "./components/Certificates";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 export default function App() {
